b18ae87a5b87bc5b3b777ea8af5167c53fae3e4e
Manan Bhatia <mananbhatia02@gmail.com>
2023-06-11 14:43:52 +0530

    Backdrop feature added

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const navToggle = document.getElementById("toggle");
 const nav = document.getElementById("navigation");
 const menuClose = document.getElementById("menu-close");
 const menuOpen = document.getElementById("menu-open");
+const backdrop = document.getElementById('backdrop')
+
 let visible = navToggle.getAttribute("data-visible") == "true";
+
 
 navToggle.addEventListener("click", () => {
     visible = !visible;
@@ -10,6 +13,8 @@ navToggle.addEventListener("click", () => {
 
     nav.classList.toggle("translateShow");
     nav.classList.toggle("translateHide");
+
+    backdrop.classList.toggle('hide')
 
     menuClose.classList.toggle("show");
     menuOpen.classList.toggle("hide");
